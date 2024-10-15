e214f440dbe198e0495a01584a4200b04f518036
Juan Charris <Juan.Charris2110@gmail.com>
2024-10-15 14:10:01 -0500

    DecimoOctavo Fix Tipo_m_routes

--- app/routes/tipo_mascota_routes.py
+++ app/routes/tipo_mascota_routes.py
@@ -1,21 +1,21 @@
 from fastapi import APIRouter, HTTPException
 from app.models.tipo_mascota_model import Tipo_mascota
-from app.controllers.roles_controller import *
+from app.controllers.tipo_mascota_controller import *
 router = APIRouter()
 
-nuevo_rol = Rolescontroller()
+nuevo_tipo_mascota = Tipo_Mascota_controller()
 
 
 @router.post("/create_tipo_mascota")
 async def create_tipo_mascota(tipo_mascota: Tipo_mascota):
-    rpta = nuevo_rol.create_tipo_mascota(tipo_mascota)
+    rpta = nuevo_tipo_mascota.create_tipo_mascota(tipo_mascota)
     return rpta
 
 
 @router.get("/get_tipo_mascota/{tipo_mascota_id}", response_model=Tipo_mascota)
 async def get_tipo_mascota(tipo_mascota_id: int):
     try:
-        rpta = nuevo_rol.get_tipo_mascota(tipo_mascota_id)
+        rpta = nuevo_tipo_mascota.get_tipo_mascota(tipo_mascota_id)
         return rpta
     except HTTPException as e:
         raise e
@@ -25,14 +25,15 @@ async def get_tipo_mascota(tipo_mascota_id: int):
 
 @router.get("/get_todos_tipo_mascota/")
 async def get_todos_tipo_mascota():
-    rpta = nuevo_rol.get_todos_tipo_mascota()
+    rpta = nuevo_tipo_mascota.get_todos_tipo_mascota()
     return rpta
 
 
 @router.put("/update_tipo_mascota/{tipo_mascota_id}")
 async def update_tipo_mascota(tipo_mascota_id: int, tipo_mascota: Tipo_mascota):
     try:
-        rpta = nuevo_rol.update_tipo_mascota(tipo_mascota_id, tipo_mascota)
+        rpta = nuevo_tipo_mascota.update_tipo_mascota(
+            tipo_mascota_id, tipo_mascota)
         return rpta
     except HTTPException as e:
         raise e
@@ -43,7 +44,7 @@ async def update_tipo_mascota(tipo_mascota_id: int, tipo_mascota: Tipo_mascota):
 @router.delete("/delete_tipo_mascota/{tipo_mascota_id}")
 async def delete_tipo_mascota(tipo_mascota_id: int):
     try:
-        rpta = nuevo_rol.delete_tipo_mascota(tipo_mascota_id)
+        rpta = nuevo_tipo_mascota.delete_tipo_mascota(tipo_mascota_id)
         return rpta
     except HTTPException as e:
         raise e
