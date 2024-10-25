dbada3eccae7286fcfc1cfac9b280bc2af2f3621
Juan Charris <Juan.Charris2110@gmail.com>
2024-10-24 22:00:52 -0500

    VigesimoSegundo Try FIX Create CollaresGPS

--- app/routes/collares_gps_routes.py
+++ app/routes/collares_gps_routes.py
@@ -3,19 +3,19 @@ from app.models.collares_gps_model import Collares_GPS
 from app.controllers.collares_gps_controller import *
 router = APIRouter()
 
-nuevo_collares_gps = CollarGPScontroller()
+nuevo_collar_gps = CollarGPScontroller()
 
 
 @router.post("/create_collar_gps")
 async def create_collar_gps(collares_gps: Collares_GPS):
-    rpta = nuevo_collares_gps.create_collar_gps(collares_gps)
+    rpta = nuevo_collar_gps.create_collar_gps(collares_gps)
     return rpta
 
 
 @router.get("/get_collargps/{collargps_id}", response_model=Collares_GPS)
 async def get_collargps(collargps_id: int):
     try:
-        rpta = nuevo_collares_gps.get_collargps(collargps_id)
+        rpta = nuevo_collar_gps.get_collargps(collargps_id)
         return rpta
     except HTTPException as e:
         raise e
@@ -25,14 +25,14 @@ async def get_collargps(collargps_id: int):
 
 @router.get("/get_collaresgps/")
 async def get_collaresgps():
-    rpta = nuevo_collares_gps.get_collaresgps()
+    rpta = nuevo_collar_gps.get_collaresgps()
     return rpta
 
 
 @router.put("/update_collargps/{collargps_id}")
 async def update_collargps(collargps_id: int, collares_gps: Collares_GPS):
     try:
-        rpta = nuevo_collares_gps.update_collargps(
+        rpta = nuevo_collar_gps.update_collargps(
             collargps_id, collares_gps)
         return rpta
     except HTTPException as e:
@@ -44,7 +44,7 @@ async def update_collargps(collargps_id: int, collares_gps: Collares_GPS):
 @router.delete("/delete_collargps/{collargps_id}")
 async def delete_collargps(collargps_id: int):
     try:
-        rpta = nuevo_collares_gps.delete_collargps(collargps_id)
+        rpta = nuevo_collar_gps.delete_collargps(collargps_id)
         return rpta
     except HTTPException as e:
         raise e
