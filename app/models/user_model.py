4369090a625cfe78f2efa06ebcfc006b2297861b
Juan Charris <Juan.Charris2110@gmail.com>
2025-01-26 12:53:28 -0500

    EN USER_MODEL SE COLOCO COMO COMENTARIO password Y id_rol

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -4,12 +4,12 @@ from pydantic import BaseModel
 class User(BaseModel):
     id: int = None
     email: str
-    password: str
+    # password: str
     nombre: str
     apellido: str
     documento: str
     telefono: str
-    id_rol: int
+    # id_rol: int
     estado: bool
 
 
